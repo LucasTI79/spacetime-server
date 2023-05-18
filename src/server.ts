a42e5e68bececa41a07de6cc1ef0718e03f5e7fe
Lucas Alves <lukasalves271@gmail.com>
2023-05-18 19:00:02 -0300

    chore: add prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
 
 const PORT = process.env.PORT || 3333
-
 app
   .listen({
     port: Number(PORT),
